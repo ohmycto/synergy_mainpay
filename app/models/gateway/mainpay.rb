088beea5be2d35f7c0a0077ca3e6af3283dd4566
Sergey Rezvanov <rezwyi@gmail.com>
2011-09-11 08:38:27 +0600

    Refused metaprogramming in the model

--- app/models/gateway/mainpay.rb
+++ app/models/gateway/mainpay.rb
@@ -1,9 +1,4 @@
 class Gateway::Mainpay < Gateway
-  URLS = { :success_url => 'success', :fail_url => 'fail', :handler_url => 'handler' }
-  URLS.each do |key, value|
-    define_method(key) { "http://[domain]/gateway/mainpay/#{value}" }
-  end
-
   preference :secret1, :string
   preference :secret2, :string
 
@@ -19,12 +14,16 @@ class Gateway::Mainpay < Gateway
     self.where(:type => self.to_s, :environment => Rails.env, :active => true).first
   end
 
-  def urls
-    URLS
-  end
-
   def input_url
     'https://partner.mainpay.ru/a1lite/input'
+  end
+
+  def desc
+    "<p>
+      <label>#{I18n.t('success_url')}:</label> http://[domain]/gateway/mainpay/success<br />
+      <label>#{I18n.t('fail_url')}:</label> http://[domain]/gateway/mainpay/fail<br />
+      <label>#{I18n.t('handler_url')}:</label> http://[domain]/gateway/mainpay/handler<br />
+    </p>"
   end
 
 end
